21e2b99b913a16cb032f53f0e1d3a524ef2ff143
eduardomagnoecon-hub <eduardo.magno.econ@gmail.com>
2025-10-20 00:03:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-st.title("Hola desde Streamlit")
 import streamlit as st
 
-st.title("🌡️ Mi primer panel en Streamlit")
-st.write("Hola, Edu. Esta es tu primera aplicación desplegada en la nube 🎉")
+st.title("🌡️ Hola desde Streamlit")
+st.write("Esta es tu primera aplicación funcionando en la nube 🎉")
 
-st.header("Demostración simple")
+st.header("Demostración interactiva")
 numero = st.slider("Selecciona un número", 1, 100, 50)
 st.write("El cuadrado de ese número es:", numero ** 2)
+
 
